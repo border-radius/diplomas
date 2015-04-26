c728c54d3729b3d6306cb0368319a2aa5d2634a1
border-radius <pharshmaster@gmail.com>
2015-04-26 16:32:52 +0300

    simple validation

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -12,30 +12,46 @@ $(document).ready(function () {
   });
 
   $('form').submit(function (e) {
-    $(e.target).find('button').attr('disabled','disabled');
-    if ($(e.target).find('input#mail').val()) {
-      $.post('/request', {
-        mail: $(e.target).find('input#mail').val(),
-        name: $(e.target).find('input#name').val(),
-        text: $(e.target).find('#text').val()
-      }).success(function () {
+    function E (e) {
+      swal('Ошибка!', e, 'error');
+    }
+
+    var form = (e.target.id === 'request') ? {
+      type: 'request',
+      phone: $(e.target).find('input#phone').val(),
+      name: $(e.target).find('input#name').val().trim()
+    } : (e.target.id === 'feedback') ? {
+      type: 'feedback',
+      mail: $(e.target).find('input#mail').val(),
+      name: $(e.target).find('input#name').val().trim(),
+      text: $(e.target).find('#text').val().trim()
+    } : {};
+
+    if (form.type === 'request' && !form.phone) {
+      E('Укажите номер телефона');
+    } else if (form.type === 'feedback' && !form.mail) {
+      E('Укажите правильный E-mail');
+    } else if (form.type === 'feedback' && !form.text) {
+      E('Укажите текст обращения');
+    } else if (!form.name) {
+      E('Укажите имя');
+    } else if (form.type) {
+      $(e.target).find('button').attr('disabled','disabled');
+
+      $.post('/request', form).success(function () {
+        if (form.type === 'request') {
+          swal("Запрос отправлен!", "Мы перезвоним Вам в течении часа", "success");
+        } else {
+          swal("Сообщение отправлено!", "Мы ответим Вам на указанный email", "success");
+        }
+
         $(e.target).find('button').removeAttr('disabled');
-        swal("Сообщение отправлено!", "Мы ответим Вам на указанный email", "success");
       }).error(function () {
         $(e.target).find('button').removeAttr('disabled');
-        swal("Ошибка", "Что-то пошло не так. Мы уже работаем над этим.", "error");
-      });      
-    } else {
-      $.post('/request', {
-        phone: $(e.target).find('input#phone').val(),
-        name: $(e.target).find('input#name').val()
-      }).success(function () {
-        $(e.target).find('button').removeAttr('disabled');
-        swal("Запрос отправлен!", "Мы перезвоним Вам в течении часа", "success");
-      }).error(function () {
-        $(e.target).find('button').removeAttr('disabled');
-        swal("Ошибка", "Что-то пошло не так. Мы уже работаем над этим.", "error");
+        E('Что-то пошло не так. Мы уже работаем над этим.');
       });
+    } else {
+      E('Неизвестная ошибка');
     }
 
     return false;
